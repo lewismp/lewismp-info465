72b00cb85960ad5423bffe88e25359b131a616fb
Marshall Lewis <lewismp@vcu.edu>
2025-01-21 09:15:13 -0500

    Transferring the pa1 file to a new .practice repository and renaming it. This was just a 'Hello World' program.

--- .practice/hello_world.js
+++ .practice/hello_world.js
@@ -1,13 +1,10 @@
 /*
 Marshall Lewis
 INFO 465 - Dr. McGarry
-Programming Assignment 1
-1/21/2025
 */
 
 // This is a simple web server that listens on port 3000 and responds with "Hello World" to all requests.
 // This code is based on the 'Hello World' example at https://nodejs.org/en/about/
-/*
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
@@ -21,5 +18,4 @@ const server = createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
+});
