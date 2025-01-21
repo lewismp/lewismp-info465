8a92ceaa7a990e9f238321e9d234ff111c832a8f
Marshall Lewis <lewismp@vcu.edu>
2025-01-21 09:16:30 -0500

    Removing the Hello World code from the pa1 file

--- pa1/pa1.js
+++ pa1/pa1.js
@@ -5,21 +5,3 @@ Programming Assignment 1
 1/21/2025
 */
 
-// This is a simple web server that listens on port 3000 and responds with "Hello World" to all requests.
-// This code is based on the 'Hello World' example at https://nodejs.org/en/about/
-/*
-const { createServer } = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
